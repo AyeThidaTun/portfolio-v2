6c0ff75626a2c55bb5c2c7f7f48a625db0fafd57
AyeThidaTun <ayethidartun.eduvalor.2023d@gmail.com>
2025-12-23 23:09:20 +0800

    feat: run biome

--- app/_components/about-me.tsx
+++ app/_components/about-me.tsx
@@ -1,9 +1,9 @@
 "use client";
+import { Icon } from "@iconify/react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { Icon } from "@iconify/react";
 
 export default function AboutMe() {
 	const [isNavigating, setIsNavigating] = useState(false);

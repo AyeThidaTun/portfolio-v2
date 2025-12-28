5d9082353fdfc96e548a14c4d8e200123817badf
AyeThidaTun <ayethidartun.eduvalor.2023d@gmail.com>
2025-12-28 23:50:44 +0800

    feat: style card and map data

--- app/projects/_components/project-cards.tsx
+++ app/projects/_components/project-cards.tsx
@@ -1,68 +1,30 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Card, CardContent } from "@/components/ui/card";
+import { cardsData } from "./project-data";
 
 export default function ProjectCard() {
 	return (
 		<section className="flex-row grid grid-cols-3 gap-x-10">
-			<Card className="w-full max-w-sm">
-				<Link href="#">
-					<div className="custom-cursor">
-						<CardContent className="row-span-2 grid-rows-subgrid space-y-5">
-							<Image
-								src={"/temp.jpg"}
-								alt={""}
-								className="object-cover aspect-video"
-								width={400}
-								height={200}
-							/>
-							<h2 className="text-md font-bold">Sustainability Task Tracker</h2>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-								Recusandae error necessitatibus ducimus, ipsam pariatur aliquid
-								similique consequuntur? Consequuntur aperiam voluptate explicabo
-								officia non, assumenda nam dolorum perferendis tempore et enim?
-							</p>
-						</CardContent>
-					</div>
-				</Link>
-			</Card>
-			<Card className="w-full max-w-sm">
-				<CardContent className="row-span-2 grid-rows-subgrid space-y-5">
-					<Image
-						src={"/temp.jpg"}
-						alt={""}
-						className="object-cover aspect-video"
-						width={400}
-						height={200}
-					/>
-					<h2 className="text-md font-bold">Sustainability Task Tracker</h2>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
-						error necessitatibus ducimus, ipsam pariatur aliquid similique
-						consequuntur? Consequuntur aperiam voluptate explicabo officia non,
-						assumenda nam dolorum perferendis tempore et enim?
-					</p>
-				</CardContent>
-			</Card>
-			<Card className="w-full max-w-sm">
-				<CardContent className="row-span-2 grid-rows-subgrid space-y-5">
-					<Image
-						src={"/temp.jpg"}
-						alt={""}
-						className="object-cover aspect-video"
-						width={400}
-						height={200}
-					/>
-					<h2 className="text-md font-bold">Sustainability Task Tracker</h2>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
-						error necessitatibus ducimus, ipsam pariatur aliquid similique
-						consequuntur? Consequuntur aperiam voluptate explicabo officia non,
-						assumenda nam dolorum perferendis tempore et enim?
-					</p>
-				</CardContent>
-			</Card>
+			{cardsData.map((card) => (
+				<Card key={card.id} className="w-full max-w-sm">
+					<Link href={card.href}>
+						<div className="custom-cursor">
+							<CardContent className="row-span-2 grid-rows-subgrid space-y-5">
+								<Image
+									src={card.src}
+									alt={""}
+									className="object-cover aspect-video"
+									width={400}
+									height={200}
+								/>
+								<h2 className="text-md font-bold">{card.title}</h2>
+								<p className="">{card.description}</p>
+							</CardContent>
+						</div>
+					</Link>
+				</Card>
+			))}
 		</section>
 	);
 }

50cc9d4b0c90658597197f0d2669078f67b40593
AyeThidaTun <ayethidartun.eduvalor.2023d@gmail.com>
2026-01-08 12:00:34 +0800

    feat: add skills page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,7 +5,7 @@ export default function Projects() {
 	return (
 		<section className="min-h-screen font-serif pb-10">
 			<div className="flex justify-center pb-10">
-				<NavigationHome href="/" />
+				<NavigationHome href="/skills" text="Skills"/>
 			</div>
 			<h3 className="text-center text-xl py-5">Projects</h3>
 			<p className="text-center">{`"Every project is a step forward, proof of how far I've come and a glimpse of where I'm going."`}</p>
---
+++ app/skills/page.tsx
@@ -0,0 +1,9 @@
+import NavigationHome from "../_components/navigation-home";
+export default function Skills() {
+  return (
+    <section className="">
+      <NavigationHome />
+      <h1 className="p-50">Skills page</h1>                     
+    </section>
+  );
+}

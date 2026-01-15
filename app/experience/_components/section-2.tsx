a657b6a08fa6ea4cbef4a848634d913d73c5fce7
AyeThidaTun <ayethidartun.eduvalor.2023d@gmail.com>
2026-01-15 17:04:44 +0800

    feat: run biome

--- app/experience/_components/section-2.tsx
+++ app/experience/_components/section-2.tsx
@@ -1,70 +1,70 @@
 import Image from "next/image";
 import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
 } from "@/components/ui/carousel";
 import { carouselImages } from "./carousel-image";
 
 export default function Section2() {
-  return (
-    <section className="">
-      <section className="bg-blue-50">
-        <div className="max-w-7xl mx-auto grid py-20 gap-10">
-          <div className="flex flex-col">
-            <h1 className="text-2xl font-semibold">
-              Co-curricular Activities (CCA)
-            </h1>
-            <p className="pt-3 pr-50">
-              I was part of the International Students Club (ISC) for 2 years
-              during poly. I was an assistant treasurer who helped manage the
-              budget and procurement of the events organized by the club. I also
-              helped organize and coordinate events for the club in roles such
-              as game master, group leader, publicity and registration.
-            </p>
-            <p className="pt-3 pr-50">
-              I also contributed to community service activities by volunteering
-              for charity events, scam awareness event, visiting elderly homes
-              and tutoring primary school kids. I find it very meaningful to
-              give back to the community through these activities.
-            </p>
-          </div>
-          <div>
-            <Carousel
-              opts={{
-                align: "start",
-              }}
-              className="w-full"
-            >
-              <CarouselContent>
-                {carouselImages.map((image) => (
-                  <CarouselItem
-                    key={image.id}
-                    className="md:basis-1/2 lg:basis-1/3"
-                  >
-                    <div className="p-1">
-                      <div className="overflow-hidden border-0 bg-transparent">
-                        <div className="relative aspect-video p-0">
-                          <Image
-                            src={image.src}
-                            alt={"CCA image"}
-                            fill
-                            className="object-cover"
-                          />
-                        </div>
-                      </div>
-                    </div>
-                  </CarouselItem>
-                ))}
-              </CarouselContent>
-              <CarouselPrevious />
-              <CarouselNext />
-            </Carousel>
-          </div>
-        </div>
-      </section>
-    </section>
-  );
+	return (
+		<section className="">
+			<section className="bg-blue-50">
+				<div className="max-w-7xl mx-auto grid py-20 gap-10">
+					<div className="flex flex-col">
+						<h1 className="text-2xl font-semibold">
+							Co-curricular Activities (CCA)
+						</h1>
+						<p className="pt-3 pr-50">
+							I was part of the International Students Club (ISC) for 2 years
+							during poly. I was an assistant treasurer who helped manage the
+							budget and procurement of the events organized by the club. I also
+							helped organize and coordinate events for the club in roles such
+							as game master, group leader, publicity and registration.
+						</p>
+						<p className="pt-3 pr-50">
+							I also contributed to community service activities by volunteering
+							for charity events, scam awareness event, visiting elderly homes
+							and tutoring primary school kids. I find it very meaningful to
+							give back to the community through these activities.
+						</p>
+					</div>
+					<div>
+						<Carousel
+							opts={{
+								align: "start",
+							}}
+							className="w-full"
+						>
+							<CarouselContent>
+								{carouselImages.map((image) => (
+									<CarouselItem
+										key={image.id}
+										className="md:basis-1/2 lg:basis-1/3"
+									>
+										<div className="p-1">
+											<div className="overflow-hidden border-0 bg-transparent">
+												<div className="relative aspect-video p-0">
+													<Image
+														src={image.src}
+														alt={"CCA image"}
+														fill
+														className="object-cover"
+													/>
+												</div>
+											</div>
+										</div>
+									</CarouselItem>
+								))}
+							</CarouselContent>
+							<CarouselPrevious />
+							<CarouselNext />
+						</Carousel>
+					</div>
+				</div>
+			</section>
+		</section>
+	);
 }

035c34a1a257a3e161c7fb73b79d9d1c8c4d09d5
AyeThidaTun <ayethidartun.eduvalor.2023d@gmail.com>
2026-01-15 17:03:59 +0800

    feat: content changes

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -8,7 +8,7 @@ export default function Projects() {
 				<NavigationHome href="/experience" text="More About Me" />
 			</div>
 			<h3 className="text-center text-xl py-5">Projects</h3>
-			<p className="text-center">{`"Every project is a step forward, proof of how far I've come and a glimpse of where I'm going."`}</p>
+			<p className="text-center">{`"Every project is a step forward, proof of how far I have come and a glimpse of where I'm going."`}</p>
 			<div className="flex justify-center py-10">
 				<ProjectCard />
 			</div>

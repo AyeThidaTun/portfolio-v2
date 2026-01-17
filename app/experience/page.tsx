87591f7fb40d4306f0f58619ed635629f68d645c
AyeThidaTun <ayethidartun.eduvalor.2023d@gmail.com>
2026-01-17 22:28:33 +0800

    feat: values section

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -2,18 +2,20 @@ import NavigationHome from "../_components/navigation-home";
 import Quote from "./_components/quote-block";
 import Section1 from "./_components/section-1";
 import Section2 from "./_components/section-2";
-// import Section3 from "./_components/section-3";
+import Section3 from "./_components/section-3";
+// import Values from "./_components/values";
 export default function MoreAboutMe() {
-	return (
-		<section className="font-serif">
-			<NavigationHome />
-			<div className="flex flex-col justify-center items-center py-10">
-				<h1 className="text-3xl">More About Me</h1>
-			</div>
-			<Section1 />
-			<Section2 />
-			<Quote />
-			{/* <Section3 /> */}
-		</section>
-	);
+  return (
+    <section className="font-serif">
+      <NavigationHome href="/projects" text="Back to Projects" />
+      <div className="flex flex-col justify-center items-center py-10">
+        <h1 className="text-3xl">More About Me</h1>
+      </div>
+      <Section1 />
+      <Section2 />
+      <Quote />
+      {/* <Values/> */}
+      <Section3 />
+    </section>
+  );
 }

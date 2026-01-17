dc7d3fc98f919f14d8c2d418123aaa0b62f1d841
AyeThidaTun <ayethidartun.eduvalor.2023d@gmail.com>
2026-01-17 23:07:46 +0800

    feat: make home page responsive

--- app/page.tsx
+++ app/page.tsx
@@ -7,56 +7,56 @@ import { Button } from "@/components/ui/button";
 import AboutMe from "./_components/about-me";
 
 export default function Home() {
-	const [showPage, setShowPage] = useState(false);
-	return (
-		<section className="font-serif">
-			<div className="min-h-screen flex flex-col text-center items-center justify-center font-serif">
-				<h3 className="px-2 lg:p-0">
-					Hi! I am Aye Thida Tun, a passionate full stack developer.
-					<br className="hidden lg:block" />
-					This website talks about my projects, passion and experiences.
-				</h3>
-				<motion.div
-					whileHover={{
-						scale: 1.1,
-						transition: { duration: 0.5 },
-					}}
-					className="py-5"
-				>
-					<Button
-						className="cursor-pointer hover:border hover:border-black hover:bg-orange-100"
-						variant="outline"
-						onClick={() => setShowPage(true)}
-					>
-						Get Started
-						<Icon icon="bx:right-arrow" width="30" height="30" />
-					</Button>
-				</motion.div>
-			</div>
-			<AnimatePresence>
-				{showPage && (
-					<motion.div
-						initial={{ y: "100%" }}
-						animate={{ y: 0 }}
-						exit={{ y: "100%" }}
-						transition={{
-							duration: 0.6,
-							ease: "easeOut",
-						}}
-						className="min-h-screen fixed inset-0 bg-white z-50 overflow-auto py-20"
-					>
-						<div className="flex items-center justify-center py-5">
-							<Button
-								className="cursor-pointer rounded-full p-5"
-								onClick={() => setShowPage(false)}
-							>
-								<Icon icon="bxs:up-arrow" width="30" height="30" />
-							</Button>
-						</div>
-						<AboutMe />
-					</motion.div>
-				)}
-			</AnimatePresence>
-		</section>
-	);
+  const [showPage, setShowPage] = useState(false);
+  return (
+    <section className="font-serif">
+      <div className="min-h-screen flex flex-col text-center items-center justify-center font-serif">
+        <h3 className="px-2 lg:p-0">
+          Hi! I am Aye Thida Tun, a passionate full stack developer.
+          <br className="hidden lg:block" />
+          This website talks about my projects, passion and experiences.
+        </h3>
+        <motion.div
+          whileHover={{
+            scale: 1.1,
+            transition: { duration: 0.5 },
+          }}
+          className="py-5"
+        >
+          <Button
+            className="cursor-pointer hover:border hover:border-black hover:bg-orange-100"
+            variant="outline"
+            onClick={() => setShowPage(true)}
+          >
+            Get Started
+            <Icon icon="bx:right-arrow" width="30" height="30" />
+          </Button>
+        </motion.div>
+      </div>
+      <AnimatePresence>
+        {showPage && (
+          <motion.div
+            initial={{ y: "100%" }}
+            animate={{ y: 0 }}
+            exit={{ y: "100%" }}
+            transition={{
+              duration: 0.6,
+              ease: "easeOut",
+            }}
+            className="min-h-screen fixed inset-0 bg-white z-50 overflow-auto py-10 lg:py-20"
+          >
+            <div className="flex items-center justify-center py-5">
+              <Button
+                className="cursor-pointer rounded-full p-5"
+                onClick={() => setShowPage(false)}
+              >
+                <Icon icon="bxs:up-arrow" width="30" height="30" />
+              </Button>
+            </div>
+            <AboutMe />
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </section>
+  );
 }

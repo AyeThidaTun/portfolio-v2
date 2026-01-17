2c5e96640d014fd86f33e805ed242d439cb5990b
AyeThidaTun <ayethidartun.eduvalor.2023d@gmail.com>
2026-01-17 22:47:34 +0800

    feat: add linkedin and gmail link

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,3 +1,5 @@
+import { Icon } from "@iconify/react";
+import Link from "next/link";
 import NavigationHome from "../_components/navigation-home";
 import Quote from "./_components/quote-block";
 import Section1 from "./_components/section-1";
@@ -8,8 +10,16 @@ export default function MoreAboutMe() {
 	return (
 		<section className="font-serif">
 			<NavigationHome href="/projects" text="Back to Projects" />
-			<div className="flex flex-col justify-center items-center py-10">
+			<div className="flex flex-col justify-center items-center py-5">
 				<h1 className="text-3xl">More About Me</h1>
+				<div className="flex flex-row gap-x-5 py-5">
+					<Link href="https://www.linkedin.com/in/ayethidatun/" target="_blank">
+						<Icon icon="skill-icons:linkedin" width="30" height="30" />
+					</Link>
+					<Link href="mailto:ayethidatun.anna@gmail.com">
+						<Icon icon="logos:google-gmail" width="30" height="30" />
+					</Link>
+				</div>
 			</div>
 			<Section1 />
 			<Section2 />

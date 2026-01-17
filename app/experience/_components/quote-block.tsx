ca6b4ed3d1e5b38d60d70bc44be536b1f93ab00f
AyeThidaTun <ayethidartun.eduvalor.2023d@gmail.com>
2026-01-17 22:27:46 +0800

    refactor: change blockquote style

--- app/experience/_components/quote-block.tsx
+++ app/experience/_components/quote-block.tsx
@@ -1,10 +1,11 @@
 export default function Quote() {
 	return (
-		<section className="flex flex-col justify-center py-20 ">
+		<section className="flex flex-col justify-center py-30">
 			<div className="max-w-7xl mx-auto">
+				<p className="text-center pb-10">This has always been my favorite quote:</p>
 				<blockquote className="text-2xl italic text-center max-w-2xl">
-					“Don&apos;t settle for average. Bring your best to the moment. Then,
-					whether it fails or succeeds, at least you know you gave all you had.”
+					“<span className="bg-blue-50">Don&apos;t settle for average. Bring your best to the moment. Then,
+					whether it fails or succeeds, at least you know you gave all you had.</span>”
 					<br />
 					<br />― Angela Bassett
 				</blockquote>
